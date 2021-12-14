827d6adb1e57360926bb7692f3fb29c50c5cadbb
sooof <sooof@me.com>
2021-12-13 22:31:32 -0800

    Episode.test.js : test #2

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -7,9 +7,17 @@ import Display from './../Display';
 
 
 test('renders without errors with no props', ()=>{
+    render(<Display show={null}/>);
 
+    const loadingMessage = screen.queryByText(/Press to Get Show Data/i);
+    // const button = screen.queryByRole('button');
+
+    expect(loadingMessage).toBeInTheDocument();
+    // expect(button).not.toBeInTheDocument();
 });
 
-test('renders Show component when the button is clicked ', ()=>{});
+test('renders Show component when the button is clicked ', ()=>{
+    render(<Display />); 
+});
 
 test('renders show season options matching your data when the button is clicked', ()=>{});
--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -4,8 +4,37 @@ import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
 
-test("renders without error", () => {});
+test("renders without error", () => {
+    render(<Episode episode={[]}/>);
+});
 
-test("renders the summary test passed as prop", ()=>{});
+test("renders the summary test passed as prop", ()=>{
+    //Arrange 1
+    const { rerender } = render(<Episode episode={[]}/>);
+    // console.log(rerender)
+    //Act 1
+    // episodes-container
+    let episodes = screen.queryAllByTestId('episodes-container');
+    //Assert 1
+    expect(episodes).toHaveLength(0);
 
-test("renders default image when image is not defined", ()=>{});
+
+
+        //Arrange 2
+        rerender(<Episode episode={
+            {id:1,name: "",image: null,season: 1,number: 1,summary: "Chapter One",runtime: 1}
+        }/>)
+    
+        //Act 3
+        // episodes = screen.queryAllByTestId('episodes-container');
+        const summary = screen.queryByText(/Chapter One/i);
+        console.log("summary", summary)
+        //Assert 3
+        expect(summary).toBeInTheDocument();
+        expect(summary).toHaveTextContent(/Chapter One/i);
+      
+});
+
+test("renders default image when image is not defined", ()=>{
+
+});
